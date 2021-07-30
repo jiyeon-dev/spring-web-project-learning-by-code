ea42f63d68aea9cfb6446621b8ec48a961e9c730
박지연 <delay@wemakeprice.com>
2021-07-30 12:24:50 +0900

    MySQL Connection TEST

---
+++ src/test/java/org/zerock/persistence/MySQLConnectionTest.java
@@ -0,0 +1,26 @@
+package org.zerock.persistence;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLConnectionTest {
+
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String AWS = "";
+    private static final String URL = "jdbc:mysql://" + AWS + ":3306/mydb?useSSL=false";
+    private static final String USER = "root";
+    private static final String PW = "admin";
+
+    @Test
+    public void testConnection() throws Exception {
+        Class.forName(DRIVER);
+        try (Connection con = DriverManager.getConnection(URL, USER, PW)) {
+            System.out.println(con);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
