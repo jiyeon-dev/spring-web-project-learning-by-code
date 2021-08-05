88860f50c84b53b3fc3c116d10459b19a5a568bd
박지연 <delay@wemakeprice.com>
2021-08-05 11:10:20 +0900

    17-2-3 CURD 작업

--- src/main/java/org/zerock/mapper/ReplyMapper.java
+++ src/main/java/org/zerock/mapper/ReplyMapper.java
@@ -1,4 +1,17 @@
 package org.zerock.mapper;
 
+import org.zerock.domain.ReplyVO;
+
+import java.util.List;
+
 public interface ReplyMapper {
+
+    public int insert (ReplyVO vo);
+
+    public ReplyVO read(Long rno);  // 특정 댓글 읽기
+
+    public int delete(Long rno);
+
+    public int update (ReplyVO reply);
+
 }
--- src/test/java/org/zerock/mapper/ReplyMapperTests.java
+++ src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -6,7 +6,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.ReplyVO;
 import org.zerock.service.BoardServiceTests;
+
+import java.util.stream.IntStream;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
@@ -14,12 +17,48 @@ public class ReplyMapperTests {
 
     private static final Logger log = Logger.getLogger(ReplyMapperTests.class);
 
+    private Long[] bnoArr = { 1L, 2L, 3L, 4L, 5L };
+
     @Autowired
     private ReplyMapper mapper;
 
     @Test
     public void testMapper() {
         log.info(mapper);
+    }
+
+    @Test
+    public void testCreate() {
+        IntStream.rangeClosed(1, 10).forEach(i -> {
+            ReplyVO vo = new ReplyVO();
+
+            vo.setBno(bnoArr[i % 5]);
+            vo.setReply("댓글 테스트 " + i);
+            vo.setReplyer("replyer " + i);
+
+            mapper.insert(vo);
+        });
+    }
+
+    @Test
+    public void testRead() {
+        ReplyVO vo = mapper.read(5L);
+        log.info(vo);
+    }
+
+    @Test
+    public void testDelete() {
+        mapper.delete(1L);
+    }
+
+    @Test
+    public void testUpdate() {
+        ReplyVO vo = mapper.read(6L);
+
+        vo.setReply("Update Reply");
+
+        int count = mapper.update(vo);
+        log.info("UPDATE COUNT: " + count);
     }
 
 }
