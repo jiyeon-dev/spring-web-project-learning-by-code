ce5c8b4f1109dd926297a1f0aaf0c788b3ebbb8c
박지연 <delay@wemakeprice.com>
2021-08-10 19:46:16 +0900

    22-2-1 이미지 파일의 판단

--- src/main/java/org/zerock/controller/UploadController.java
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -1,5 +1,8 @@
 package org.zerock.controller;
 
+import net.coobird.thumbnailator.Thumbnailator;
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.name.Rename;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -8,7 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -23,6 +27,17 @@ public class UploadController {
         Date date = new Date();
         String str = sdf.format(date);
         return str.replace("-", File.separator);
+    }
+
+    private boolean checkImageType(File file) {
+        try {
+            String contentType = Files.probeContentType(file.toPath());
+            log.info(contentType);
+            return contentType.startsWith("image");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     @GetMapping("/uploadForm")
@@ -84,11 +99,18 @@ public class UploadController {
             UUID uuid = UUID.randomUUID();
             uploadFileName = uuid.toString() + "_" + uploadFileName;
 
-//            File saveFile = new File(uploadFolder, uploadFileName);
-            File saveFile = new File(uploadPath, uploadFileName);
-
             try {
+                //            File saveFile = new File(uploadFolder, uploadFileName);
+                File saveFile = new File(uploadPath, uploadFileName);
                 multipartFile.transferTo(saveFile);
+
+                // check image type file
+                if (checkImageType(saveFile)) {
+                    FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+                    Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+
+                    thumbnail.close();
+                }
             } catch (Exception e) {
                 log.error(e.getMessage());
             }
