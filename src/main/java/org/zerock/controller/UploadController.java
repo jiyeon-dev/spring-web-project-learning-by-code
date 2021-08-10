88ed0e8570deabd5aaa90d80736a5c34f124f6b6
박지연 <delay@wemakeprice.com>
2021-08-10 16:33:16 +0900

    21-2-1 MultipartFile 타입

--- src/main/java/org/zerock/controller/UploadController.java
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -1,16 +1,44 @@
 package org.zerock.controller;
 
-import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 @Controller
-@Log
 public class UploadController {
+
+    private static final Logger log = LoggerFactory.getLogger(UploadController.class);
 
     @GetMapping("/uploadForm")
     public void uploadForm() {
         log.info("upload form");
+    }
+
+    @PostMapping("uploadFormAction")
+    public void uploadFormAction(MultipartFile[] uploadFile, Model model) {
+
+        String uploadFolder = "C:\\upload";
+
+        for (MultipartFile multipartFile : uploadFile) {
+            log.info("------------------------------------------");
+            log.info("Upload File Name: " + multipartFile.getOriginalFilename());
+            log.info("Upload File Size: " + multipartFile.getSize());
+
+            File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+
+            try {
+                multipartFile.transferTo(saveFile);
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        }
+
     }
 
 }
