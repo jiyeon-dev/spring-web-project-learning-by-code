1cf2562bb3f5a9d0a795d0a61a0768f2bc59d738
박지연 <delay@wemakeprice.com>
2021-08-11 19:42:15 +0900

    29 잘못 업로드된 파일 삭제(스케쥴러-Quartz라이브러리)

--- src/main/java/org/zerock/mapper/BoardAttachMapper.java
+++ src/main/java/org/zerock/mapper/BoardAttachMapper.java
@@ -13,5 +13,7 @@ public interface BoardAttachMapper {
     public List<BoardAttachVO> findByBno(Long bno);
 
     public void deleteAll(Long bno);  // 게시물 삭제시 관련 첨부파일 모두 삭제
+    
+    public List<BoardAttachVO> getOldFiles();  // 어제 등록된 파일 조회
 
 }
