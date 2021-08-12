12c8f942e82d9b9f02e665cbdc29dd5a0083a402
박지연 <delay@wemakeprice.com>
2021-08-12 20:30:18 +0900

    36-5 JDBC를 이용하는 Java 설정

--- src/main/java/org/zerock/config/RootConfig.java
+++ src/main/java/org/zerock/config/RootConfig.java
@@ -1,9 +1,51 @@
 package org.zerock.config;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = { "org.zerock" })
+@MapperScan(basePackages = { "org.zerock.mapper" })
+@PropertySource(value = { "classpath:config/jdbc.properties" })
 public class RootConfig {
+
+    @Value("${jdbc.driver}")
+    private String driverClassName;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String userName;
+    @Value("${jdbc.password}")
+    private String password;
+
+
+    @Bean
+    public DataSource dataSource() {
+
+        BasicDataSource dataSource = new BasicDataSource();
+
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource());
+        return sqlSessionFactory.getObject();
+    }
+
 }
--- src/main/java/org/zerock/config/SecurityConfig.java
+++ src/main/java/org/zerock/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.zerock.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,23 +14,42 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.zerock.security.CustomLoginSuccessHandler;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
 
-    /**
-     * 테스트 계정 생성
-     * @param auth
-     * @throws Exception
-     */
+    @Autowired
+    private DataSource dataSource;
+
+//    /**
+//     * 테스트 계정 생성
+//     * @param auth
+//     * @throws Exception
+//     */
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        log.info("configure..................................");
+//
+//        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
+//        auth.inMemoryAuthentication().withUser("member").password("$2a$10$ATt/KOfHtXzm.IbdxuAcpeNlJeN5nedl6gw0LFy3wwBON4oDuI3uG").roles("MEMBER");
+//    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        log.info("configure..................................");
+        log.info("configure JDBC ..................................");
 
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
-        auth.inMemoryAuthentication().withUser("member").password("$2a$10$ATt/KOfHtXzm.IbdxuAcpeNlJeN5nedl6gw0LFy3wwBON4oDuI3uG").roles("MEMBER");
+        String queryUser = "SELECT userid, userpw, enabled FROM tbl_member WHERE userid = ? ";
+        String queryDetails = "SELECT userid, auth FROM tbl_member_auth WHERE userid = ? ";
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery(queryUser)
+                .authoritiesByUsernameQuery(queryDetails);
     }
 
     @Bean
