a12c94a463397d55a493a5cbc353ddfb1e02b2cf
박지연 <delay@wemakeprice.com>
2021-08-12 16:21:32 +0900

    32-1 JDBC를 이용하기 위한 테이블 설정

---
+++ src/test/java/org/zerock/persistence/MySQLConnectionTest.java
@@ -0,0 +1,31 @@
+package org.zerock.persistence;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class MySQLConnectionTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testConnection() {
+
+        try (Connection conn =  dataSource.getConnection()) {
+            System.out.println(conn);
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+
+    }
+
+}
