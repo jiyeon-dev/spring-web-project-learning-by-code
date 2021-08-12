a8ddbb8ffd1a2c5033544a9516723abadbd3c934
박지연 <delay@wemakeprice.com>
2021-08-12 11:54:59 +0900

    30-2 시큐리티가 필요한 URL 설계

---
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,30 @@
+package org.zerock.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample/*")
+public class SampleController {
+
+    private static final Logger log = LoggerFactory.getLogger(SampleController.class);
+
+    @GetMapping("/all")
+    public void doAll() {
+        log.info("do all ca access everybody");
+    }
+
+    @GetMapping("/member")
+    public void doMember() {
+        log.info("logined member");
+    }
+
+    @GetMapping("/admin")
+    public void doAdmin() {
+        log.info("admin only");
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/sample/admin.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>/sample/admin page</h1>
+</body>
+</html>
